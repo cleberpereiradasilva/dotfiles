132466f0f186f0d5203413de178b9b9d0c146d4e
Paulo Roberto <paulo.pinda@gmail.com>
2020-02-04 22:07:47 -0300

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+echo "Installing i3wm configuration..."
+cp ./i3/config ~/.config/i3/
+
+echo "Installing polybar configuration..."
+cp -rf polybar/* ~/.config/polybar
+
+echo "Installing neovim plug manager..."
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Installing neovim configuration..."
+cp ./vim/init.vim ./vim/coc-settings.json ~/.config/nvim/
+nvim -E -c PlugInstall -c q
+
+echo "Installing neovim transparent airline..."
+cp ./vim/transparent.vim ~/.vim/plugged/vim-airline-themes/autoload/airline/themes/transparent.vim
+
+echo "Copying wallpaper directory..."
+cp -rf ./wallpaper ~/
