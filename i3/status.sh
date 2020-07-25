1656d678b7070e9b4788846d6e0f285e3180bec1
noct <cleber_pesilva@yahoo.com.br>
2020-07-25 00:19:30 -0300

    Remove files unused

--- i3/status.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/home/paulo/.pyenv/shims/bumblebee-status -m nic cpu memory publicip bluetooth date time battery taskwarrior -p nic.format='{ip} {ssid}' root.path=/ time.format="%H:%M" date.format="%a, %b %d %Y" nic.exclude=lo,docker,vboxnet,veth,br,ppp0,enp3s0 -t iceberg-dark-powerline
--- polybar/scripts/date.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from babel.dates import format_datetime
-
-date_now = datetime.now()
-formated_date = format_datetime(date_now)
-
-print(formated_date)
